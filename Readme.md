5b05b3d833c54e43f1c4c7bc492c8386197e0cf6
pepijngriffioen <pepijngriffioen@hotmail.com>
2021-02-23 16:54:22 +0100

    First commit with the files.

---
+++ Readme.md
@@ -0,0 +1,26 @@
+## Readme for BibTexConverter
+
+Using the bibtexparser.
+
+The Mendeley online export function had problems with the generation of some citationkeys. I used the bibtexparser package to write a small python program to format my bibliography easier for my Master's thesis. It turned out that the package also had an option to format some encoding text, which I corrected manually in the past. A win-win so to say.
+
+
+## Usage
+
+create a virtual environment using python and source it. 
+```
+python3 -m venv testVenv
+source testVenv/bin/activate
+pip install -r requirements.txt
+```
+
+One can run the program as follows where input.bib is the input bibliography and output.bib is the output bibliography.
+```
+python3 bib-sort.py input.bib output.bib
+```
+
+## Adding keys
+
+The problem I had was that for some reason Mendeley did not export keys. So I had to manually add them, as this was a straightforward process it could be easily programmed. In the if statement in the middle of the code is some logic defined to change comments into correct bib entries. One can change it or define it for itself. 
+
+The aim in the future is to make this meta driven, so there will be a separate file containing the search criteria and key results.
---
+++ bib-sort.py
@@ -0,0 +1,58 @@
+import bibtexparser
+from bibtexparser.bparser import BibTexParser
+from bibtexparser.customization import homogenize_latex_encoding
+from bibtexparser.bwriter import BibTexWriter
+from bibtexparser.bibdatabase import BibDatabase
+from bibtexparser.bibdatabase import STANDARD_TYPES
+import argparse
+
+
+def addCitationKey(entry, newKey):
+    ix = entry.find('{')
+    return entry[:ix+1] + newKey + entry[ix+1:]
+
+def checkType(entry):
+    attx = entry.find('@')
+    ix = entry.find('{')
+    typeBib = entry[attx+1:ix]
+    return typeBib in STANDARD_TYPES
+
+def main():
+    argsParser = argparse.ArgumentParser()
+    argsParser.add_argument('path', type=str)
+    argsParser.add_argument('output_path', type=str)
+    args = argsParser.parse_args()
+
+    parser = BibTexParser()
+    parser.customization = homogenize_latex_encoding
+
+    with open(args.path, 'r') as bib_file:
+        bib_database = bibtexparser.load(bib_file, parser=parser)
+
+    #Addin the keys - future to do is add using a json file - very meta driven.
+    for comment in bib_database.comments:
+        parse = False
+        if "{Object Management Group}" in comment:
+            newEntry = addCitationKey(comment, "OMG")
+            parse = True
+        elif "{Javier J. Gutiérrez and Clémentine Nebut and María J. Escalona and Manuel Mejías and Isabel M. Ramos}" in comment:
+            newEntry = addCitationKey(comment, "Gutierrez2008")
+            parse = True
+        
+        if parse:
+            if checkType(newEntry):
+                try:
+                    bib_database = bibtexparser.loads(newEntry, parser)
+                    bib_database.comments.remove(comment)
+                except:
+                    pass
+            else:
+                print("Failed because of a wrong type for {}".format(newEntry))
+
+    writer = BibTexWriter()
+    writer.indent = '\t'
+    with open(args.output_path,'w') as bibfile:
+        bibfile.write(writer.write(bib_database))
+
+if __name__ == '__main__':
+    main()
