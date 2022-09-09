94315cdb0057f55abb90270cfa4cee8eab9225b6
pepijngriffioen <pepijngriffioen@hotmail.com>
2022-09-09 11:25:02 +0200

    added black formatting

--- bib-sort.py
+++ bib-sort.py
@@ -8,63 +8,80 @@ import argparse
 
 
 def addCitationKey(entry, newKey):
-    ix = entry.find('{')
-    return entry[:ix+1] + newKey + entry[ix+1:]
+    ix = entry.find("{")
+    return entry[: ix + 1] + newKey + entry[ix + 1 :]
+
 
 def checkType(entry):
-    attx = entry.find('@')
-    ix = entry.find('{')
-    typeBib = entry[attx+1:ix]
+    attx = entry.find("@")
+    ix = entry.find("{")
+    typeBib = entry[attx + 1 : ix]
     return typeBib in STANDARD_TYPES
 
-def retrieve_author(comment_entry:str) -> str:
+
+def retrieve_author(comment_entry: str) -> str:
     """Retrieve the author from an entry as a comment.
-    
+
     Args:
         - comment_entry (str): the comment to search through.
-    
+
     Returns:
         - author (str): the author string from the comment.
     """
     author_word_index = comment_entry.index("author")
     start_bracket = comment_entry[author_word_index:].index("{")
     close_bracket = comment_entry[author_word_index:].index("}")
-    author = comment_entry[author_word_index+start_bracket+1:author_word_index+close_bracket]
+    author = comment_entry[
+        author_word_index + start_bracket + 1 : author_word_index + close_bracket
+    ]
     return author
+
 
 def main():
     argsParser = argparse.ArgumentParser()
-    argsParser.add_argument('path', type=str)
-    argsParser.add_argument('output_path', type=str)
+    argsParser.add_argument("path", type=str)
+    argsParser.add_argument("output_path", type=str)
     args = argsParser.parse_args()
 
     parser = BibTexParser()
     parser.customization = homogenize_latex_encoding
-    with open(args.path, 'r') as bib_file:
+    with open(args.path, "r") as bib_file:
         bib_database = bibtexparser.load(bib_file, parser=parser)
 
     removeComments = []
-    #Addin the keys - future to do is add using a json file - very meta driven.
+    # Addin the keys - future to do is add using a json file - very meta driven.
     for comment in bib_database.comments:
         parse = False
         print(comment)
         if "{Object Management Group}" in comment:
             newEntry = addCitationKey(comment, "OmgUML")
             parse = True
-        elif "{Javier J. Gutiérrez and Clémentine Nebut and María J. Escalona and Manuel Mejías and Isabel M. Ramos}" in comment:
+        elif (
+            "{Javier J. Gutiérrez and Clémentine Nebut and María J. Escalona and Manuel Mejías and Isabel M. Ramos}"
+            in comment
+        ):
             newEntry = addCitationKey(comment, "Gutierrez2008")
             parse = True
-        elif "{Han van der Aa and Claudio Di Ciccio and Henrik Leopold and Hajo A. Reijers}" in comment:
+        elif (
+            "{Han van der Aa and Claudio Di Ciccio and Henrik Leopold and Hajo A. Reijers}"
+            in comment
+        ):
             newEntry = addCitationKey(comment, "vanderAa2019")
             parse = True
-        elif "{João Carlos de A.R. Gonçalves and Flávia Maria Santoro and Fernanda Araujo Baião}" in comment:
+        elif (
+            "{João Carlos de A.R. Gonçalves and Flávia Maria Santoro and Fernanda Araujo Baião}"
+            in comment
+        ):
             newEntry = addCitationKey(comment, "Goncalves2011")
             parse = True
         elif "{Cristina Venera Geambaşu}" in comment:
-            newEntry = addCitationKey(comment,"geambasu2012")
+            newEntry = addCitationKey(comment, "geambasu2012")
             parse = True
-        elif "{Marie-Catherine De Marneffe and Bill Maccartney and Christopher D Manning}" in comment:
-            newEntry = addCitationKey(comment,"marneffe2006")
+        elif (
+            "{Marie-Catherine De Marneffe and Bill Maccartney and Christopher D Manning}"
+            in comment
+        ):
+            newEntry = addCitationKey(comment, "marneffe2006")
             parse = True
         if parse:
             if checkType(newEntry):
@@ -80,9 +97,10 @@ def main():
         bib_database.comments.remove(rmComment)
 
     writer = BibTexWriter()
-    writer.indent = '\t'
-    with open(args.output_path,'w') as bibfile:
+    writer.indent = "\t"
+    with open(args.output_path, "w") as bibfile:
         bibfile.write(writer.write(bib_database))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
